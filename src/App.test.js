adc7315213ab294d1fd82670a2748d1051c74bde
wade.zhong <756059388@qq.com>
2020-03-08 16:08:05 +0800

    lesson1

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+it("renders without crashing", () => {
+    // const div = document.createElement('div');
+    // const container = div.getElementsByClassName('App');
+    // expect(container.length).toBe(1);
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App').length).toBe(1);
 });
