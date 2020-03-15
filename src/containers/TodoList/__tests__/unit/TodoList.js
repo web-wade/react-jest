04d57207b5b09d4edc139e906afc3b02c7055b62
wade.zhong <756059388@qq.com>
2020-03-15 11:05:56 +0800

    修改

--- src/containers/TodoList/__tests__/unit/TodoList.js
+++ src/containers/TodoList/__tests__/unit/TodoList.js
@@ -2,38 +2,98 @@ import React from "react";
 import { shallow } from "enzyme";
 import TodoList from "../../index";
 
-it('TodoList 初始化列表为空', () => {
-    const wrapper = shallow(<TodoList/>);
-    expect(wrapper.state('undoList')).toEqual([])
+it("TodoList 初始化列表为空", () => {
+    const wrapper = shallow(<TodoList />);
+    expect(wrapper.state("undoList")).toEqual([]);
 });
 
-it('TodoList 应该给Header传递一个增加undoList内容的方法', () => {
-    const wrapper = shallow(<TodoList/>);
-    const Header = wrapper.find('Header');
-    expect(Header.prop('addUndoItem')).toBe(wrapper.instance().addUndoItem)
-});  
+it("TodoList 应该给Header传递一个增加undoList内容的方法", () => {
+    const wrapper = shallow(<TodoList />);
+    const Header = wrapper.find("Header");
+    expect(Header.prop("addUndoItem")).toBe(wrapper.instance().addUndoItem);
+});
 
-it('当header回车是时，undolist应该新增内容', () => {
-    const wrapper = shallow(<TodoList/>);
-    const Header = wrapper.find('Header');
-    const addFunc = Header.prop('addUndoItem');
-    addFunc('学习React') 
-    expect(wrapper.state('undoList').length).toBe(1)
-    expect(wrapper.state('undoList')[0]).toBe('学习React')
-}); 
+it("当header回车是时，undolist应该新增内容", () => {
+    const wrapper = shallow(<TodoList />);
+    const Header = wrapper.find("Header");
+    const addFunc = Header.prop("addUndoItem");
+    addFunc("学习React");
+    expect(wrapper.state("undoList").length).toBe(1);
+    expect(wrapper.state("undoList")[0]).toEqual({
+        status: "div",
+        value: "学习React"
+    });
+});
 
-it("todoList应该给未完成的列表传递list数据和删除方法", () => {
-    const wrapper = shallow(<TodoList/>);
+it("todoList应该给未完成的列表传递list数据和删除方法和changeStatus和handleBlur,valueChange", () => {
+    const wrapper = shallow(<TodoList />);
     const UndoList = wrapper.find("UndoList");
-    expect(UndoList.prop('list')).toBeTruthy();
-    expect(UndoList.prop('deleteItem')).toBeTruthy();
+    expect(UndoList.prop("list")).toBeTruthy();
+    expect(UndoList.prop("deleteItem")).toBeTruthy();
+    expect(UndoList.prop("changeStatus")).toBeTruthy();
+    expect(UndoList.prop("handleBlur")).toBeTruthy();
+    expect(UndoList.prop("valueChange")).toBeTruthy();
 });
 
 it("当delete执行时，undolist应该删除内容", () => {
-    const wrapper = shallow(<TodoList/>);
+    const wrapper = shallow(<TodoList />);
     wrapper.setState({
-        undoList:["1",'2']
-    })
-    wrapper.instance().deleteItem(1)
-    expect(wrapper.state('undoList')).toEqual(["1"]);
+        undoList: [
+            {
+                status: "div",
+                value: "学习React"
+            },
+            {
+                status: "div",
+                value: "学习React"
+            }
+        ]
+    });
+    wrapper.instance().deleteItem(1);
+    expect(wrapper.state("undoList")).toEqual([
+        {
+            status: "div",
+            value: "学习React"
+        }
+    ]);
+});
+
+it("changeStatus方法被调用，undoList数据项被删除", () => {
+    const wrapper = shallow(<TodoList />);
+    const data = [
+        {
+            status: "div",
+            value: "学习React"
+        },
+        {
+            status: "div",
+            value: "学习React"
+        }
+    ];
+    wrapper.setState({
+        undoList: data
+    });
+    wrapper.instance().changeStatus(1);
+    expect(wrapper.state("undoList")[1]).toEqual({
+        ...data[1],
+        status: "input"
+    });
+});
+
+it("valueChange方法被调用，undoList数据项被修改", () => {
+    const wrapper = shallow(<TodoList />);
+    const data = [
+        {
+            status: "input",
+            value: "学习React"
+        }
+    ];
+    wrapper.setState({
+        undoList: data
+    });
+    wrapper.instance().valueChange(0, "11111");
+    expect(wrapper.state("undoList")[0]).toEqual({
+        ...data[0],
+        value: "11111"
+    });
 });
--- src/containers/TodoList/index.js
+++ src/containers/TodoList/index.js
@@ -15,20 +15,56 @@ export default class TodoList extends Component {
         return (
             <div>
                 <Header addUndoItem={this.addUndoItem} />
-                <UndoList deleteItem={this.deleteItem} list={this.state.undoList}/>
+                <UndoList
+                    changeStatus={this.changeStatus}
+                    deleteItem={this.deleteItem}
+                    handleBlur={this.handleBlur}
+                    list={this.state.undoList}
+                    valueChange={this.valueChange}
+                />
             </div>
         );
     }
 
     addUndoItem = value => {
         this.setState({
-            undoList: [...this.state.undoList, value]
+            undoList: [
+                ...this.state.undoList,
+                {
+                    status: "div",
+                    value
+                }
+            ]
         });
     };
 
     deleteItem = index => {
         const list = [...this.state.undoList];
-        list.splice(index,1)
+        list.splice(index, 1);
+        this.setState({
+            undoList: list
+        });
+    };
+
+    changeStatus = index => {
+        const list = [...this.state.undoList];
+        list[index].status = "input";
+        this.setState({
+            undoList: list
+        });
+    };
+
+    handleBlur = index => {
+        const list = [...this.state.undoList];
+        list[index].status = "div";
+        this.setState({
+            undoList: list
+        });
+    };
+
+    valueChange = (index, value) => {
+        const list = [...this.state.undoList];
+        list[index].value = value;
         this.setState({
             undoList: list
         });
